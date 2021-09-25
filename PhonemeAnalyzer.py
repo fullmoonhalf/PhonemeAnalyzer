5dcd503c9405fe8e3f4e396582de3e958e244638
fullmoonhalf <fullmoonhalf@users.noreply.github.com>
2021-09-25 20:53:13 +0900

    メインシーケンスのスケルトン

---
+++ PhonemeAnalyzer.py
@@ -0,0 +1,14 @@
+import argparse
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='test')
+    args = parser.parse_args()
+    return args
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    main(parse_args())
